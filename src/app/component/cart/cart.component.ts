5b7a970d357bb50cf9341ea398d6a237f9dfdbc5
Nikola Bankovic <nikola.bankovic@fer.hr>
2024-05-31 14:59:10 +0200

    Add shopping cart

--- src/app/component/cart/cart.component.ts
+++ src/app/component/cart/cart.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {Cart, CartService} from '../../service/cart.service';
+import {Cart, CartItem, CartService} from '../../service/cart.service';
 import {FormsModule} from "@angular/forms";
 import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
-  selector: 'app-shopping-cart',
+  selector: 'app-cart',
   standalone: true,
   imports: [
     FormsModule,
@@ -16,9 +16,10 @@ import {NgForOf, NgIf} from "@angular/common";
 })
 export class CartComponent implements OnInit {
   cart: Cart | undefined;
+  totalCost: number = 0;
 
   newProductId: number = 0;
-  newQuantity: number = 1;
+  newQuantity: number = 0;
 
   constructor(private cartService: CartService) { }
 
@@ -29,26 +30,51 @@ export class CartComponent implements OnInit {
   loadCart() {
     this.cartService.viewCart().subscribe(cart => {
       this.cart = cart;
+      this.calculateTotalCost();
     });
   }
 
   addItem() {
     this.cartService.addItem(this.newProductId, this.newQuantity).subscribe(cart => {
       this.cart = cart;
+      this.calculateTotalCost()
       this.newProductId = 0;
-      this.newQuantity = 1;
+      this.newQuantity = 0;
     });
   }
 
   removeItem(productId: number) {
     this.cartService.removeItem(productId).subscribe(cart => {
       this.cart = cart;
+      this.calculateTotalCost();
     });
   }
 
   clearCart() {
     this.cartService.clearCart().subscribe(cart => {
       this.cart = cart;
+      this.totalCost = 0;
     });
+  }
+  incrementQuantity(item: CartItem) {
+    this.cartService.addItem(item.product.id, 1).subscribe(cart => {
+      this.cart = cart;
+      this.calculateTotalCost()
+    });
+  }
+
+  decrementQuantity(item: CartItem) {
+    if (item.quantity > 1) {
+      this.cartService.addItem(item.product.id, -1).subscribe(cart => {
+        this.cart = cart;
+        this.calculateTotalCost()
+      });
+    }
+  }
+
+  calculateTotalCost() {
+    if (this.cart) {
+      this.totalCost = this.cart.cartItemList.reduce((total, item) => total + (item.product.price * item.quantity), 0);
+    }
   }
 }
